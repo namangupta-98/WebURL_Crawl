d0c683760c423c3e986263802959fc26fc5542de
Naman Gupta <33204350+imstark@users.noreply.github.com>
2019-09-04 15:02:49 +0530

    Add files via upload

---
+++ dmoz/pipelines.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import psycopg2
+
+
+class DmozPipeline(object):
+    def open_spider(self, spider):
+        hostname = 'localhost'
+        username = 'postgres'
+        password = 'postgre'
+        database = 'dmoz'
+        self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+        self.cur = self.connection.cursor()
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
+
+    def process_item(self, item, spider):
+        self.cur.execute("INSERT INTO links (link) VALUES ({})".format("'https://dmoz-odp.org/"+item['link']+"'"))
+        self.connection.commit()
+        return item
